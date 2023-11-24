a35c6d7299d073773f9501ecb306f8a4c09ecb26
BellaZhukova <148293805+BellaZhukova@users.noreply.github.com>
2023-11-24 20:27:08 +0700

    Add files via upload

---
+++ snippets/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path, include
+from rest_framework import renderers
+from rest_framework.urlpatterns import format_suffix_patterns
+
+from . import views
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from . import views
+
+router = DefaultRouter()
+router.register(r'snippets', views.SnippetViewSet)
+router.register(r'users', views.UserViewSet)
+
+urlpatterns = [
+   path('', include(router.urls)),
+]
+
+
+
+
